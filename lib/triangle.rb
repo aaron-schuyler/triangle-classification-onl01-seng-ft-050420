5fd2dfc2274585c120b3d17129762b572dd271bc
Aaron Schuyler <asky28@gmail.com>
2020-05-20 20:12:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,15 @@
 class Triangle
-  # write code here
+  attr_accessor :one, :two, :three
+  def initialize(one, two, three)
+    @one = one
+    @two = two
+    @three = three
+  end 
+  def kind 
+    if 
+  end
+end
+
+def TriangleError < StandardError
+  
 end
